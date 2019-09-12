8d458eab0f375667b03356c2c9ed9022ac0e48ab
Ankan Sikdar <ankansikdar@gmail.com>
2019-09-12 16:00:48 +0530

    Created Card.js

---
+++ src/Card.js
@@ -0,0 +1,13 @@
+import React from 'react'
+
+const Card = () => {
+    return (
+        <div>
+            <h3>Name: Ankan Sikdar</h3>
+            <p>Sex: Male</p>
+            <p>Birth Year: 1999</p>
+        </div>
+    )
+}
+
+export default Card;
