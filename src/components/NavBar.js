2e7386fb746e7c557fbba99c5fa05b3b7a7f818b
Ankan Sikdar <ankansikdar@gmail.com>
2019-09-13 20:49:30 +0530

    Added navbar link to home

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -9,7 +9,7 @@ const NavBar = () => {
             <h1 className="mt2 mb0 ttu baskerville i fw1 f1">Star Wars Wiki</h1>
             <h2 className="mt2 mb0 f6 fw4 tracked">Details of Characters</h2>
             <nav className="bt bb tc center mt4">
-                <a className="f6 f5-l link bg-animate black-80 hover-bg-lightest-blue dib pa3 ph4-l" href="/">Home</a>
+                <a className="f6 f5-l link bg-animate black-80 hover-bg-lightest-blue dib pa3 ph4-l" href="#navbar">Home</a>
                 <a className="f6 f5-l link bg-animate black-80 hover-bg-light-yellow dib pa3 ph4-l" href="#footer">Contact</a>
             </nav>
         </header>
