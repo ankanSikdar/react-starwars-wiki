297f38263b65214bcc8cfca14989b2ef376993c3
Ankan Sikdar <ankansikdar@gmail.com>
2019-09-13 13:55:50 +0530

    Updates

---
+++ precache-manifest.62cccd5840a51c756c826016de815501.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "718730221f44e5cfbd7e1efc1e1d6052",
+    "url": "/react-starwars-wiki/index.html"
+  },
+  {
+    "revision": "e48fcca9f5c5c22c54bf",
+    "url": "/react-starwars-wiki/static/css/main.0c907582.chunk.css"
+  },
+  {
+    "revision": "fb33f7ec29fb57b33b93",
+    "url": "/react-starwars-wiki/static/js/2.e237e7d7.chunk.js"
+  },
+  {
+    "revision": "e48fcca9f5c5c22c54bf",
+    "url": "/react-starwars-wiki/static/js/main.909de14e.chunk.js"
+  },
+  {
+    "revision": "60577fc01bbd366e28d4",
+    "url": "/react-starwars-wiki/static/js/runtime~main.989eb049.js"
+  }
+]);
