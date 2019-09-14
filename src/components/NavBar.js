33d5dcb49689de4e66756d13e8e4af52e9770cad
Ankan Sikdar <ankansikdar@gmail.com>
2019-09-14 12:56:05 +0530

    Changed sub-title to List of Characters

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -7,7 +7,7 @@ const NavBar = () => {
                 <img className="pa3 w3 h3" alt="LightSaber ;)" src="https://image.flaticon.com/icons/svg/2034/2034751.svg"></img>
             </a>
             <h1 className="mt2 mb0 ttu baskerville i fw1 f1">Star Wars Wiki</h1>
-            <h2 className="mt2 mb0 f6 fw4 tracked">Details of Characters</h2>
+            <h2 className="mt2 mb0 f6 fw4 tracked">List of Characters</h2>
             <nav className="bt bb tc center mt4">
                 <a className="f6 f5-l link bg-animate black-80 hover-bg-lightest-blue dib pa3 ph4-l" href="#navbar">Home</a>
                 <a className="f6 f5-l link bg-animate black-80 hover-bg-light-yellow dib pa3 ph4-l" href="#footer">Contact</a>
